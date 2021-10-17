660ad7b211a85919ad3305425c1d449f932733d5
liwenbo <17751002310@189.cn>
2021-10-17 21:05:01 +0800

    跳出多重循环

---
+++ src/main/java/com/wenbo/loop/MutiLoopBreak.java
@@ -0,0 +1,46 @@
+package com.wenbo.loop;
+
+/**
+ * @author: liwenbo
+ * @date: 2021-10-17 17:00:47
+ * @desc: 嵌套循环 break
+ */
+public class MutiLoopBreak {
+    public static void main(String[] args) {
+//        breakOneLoop();
+        breakMultiLoop();
+    }
+
+    private static void breakMultiLoop() {
+        int i = 0;
+        int j = 0;
+        flag:
+        for (i = 0; i < 10; i++) {
+            for (j = 0; j < 10; j++) {
+                if (i == 5 && j == 6) {
+                    System.out.println("i:" + i + " j:" + j);
+                    System.out.println("----------------------");
+                    break flag;
+                }
+                System.out.println("i:" + i + " j:" + j);
+            }
+        }
+        System.out.println("i:" + i + " j:" + j);
+    }
+
+    private static void breakOneLoop() {
+        int i = 0;
+        int j = 0;
+        for (i = 0; i < 10; i++) {
+            for (j = 0; j < 10; j++) {
+                if (i == 5 && j == 6) {
+                    System.out.println("i:" + i + " j:" + j);
+                    System.out.println("----------------------");
+                    break;
+                }
+                System.out.println("i:" + i + " j:" + j);
+            }
+        }
+        System.out.println("i:" + i + " j:" + j);
+    }
+}
