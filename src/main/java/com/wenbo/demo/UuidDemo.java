53514b13644fabc0c76dee923c41742a65021abe
liwenbo <17751002310@189.cn>
2021-10-27 22:58:34 +0800

    UUID Demo

---
+++ src/main/java/com/wenbo/demo/UuidDemo.java
@@ -0,0 +1,18 @@
+package com.wenbo.demo;
+
+import java.util.UUID;
+
+/**
+ * @author: liwenbo
+ * @date: 2021-10-27 19:29:22
+ * 
+ * @desc: 
+ */
+public class UuidDemo {
+    public static void main(String[] args) {
+        System.out.println(UUID.randomUUID());
+        System.out.println(UUID.randomUUID());
+        System.out.println(UUID.randomUUID());
+        System.out.println(UUID.randomUUID());
+    }
+}
